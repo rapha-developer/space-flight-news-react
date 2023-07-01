f3ae35d4a8825b80e0c2f14377a609d650d0cb19
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-01 12:46:17 -0300

    [Header step:1] create search bar with debounce function

---
+++ src/components/Header/HeaderSearchBar.tsx
@@ -0,0 +1,27 @@
+import type { ChangeEvent } from 'react'
+import _debounce from 'lodash/debounce'
+import { useCallback } from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+export const HeaderSearchBar = () => {
+    const onChangeHandler = useCallback(
+        _debounce((event: ChangeEvent<HTMLInputElement>) => {
+            console.log({ query: event.target.value })
+        }, 500),
+        []
+    )
+    return (
+        <div className="flex grow">
+            <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <MagnifyingGlassIcon className='h-4 w-4 text-gray-500' />
+                </div>
+                <input
+                    className='block p-2.5 pl-10 w-full z-20 text-lg text-gray-900 border-none focus-visible:outline-none'
+                    type="search" 
+                    placeholder='Search'
+                    onChange={onChangeHandler}
+                />
+            </div>
+        </div>
+    )
+}
