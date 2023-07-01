d87f2d1a6034fdaa0d875e2e6978aec425629625
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-01 11:37:38 -0300

    [Header step:1] Create and export HeaderLogo

---
+++ src/components/Header/HeaderLogo.tsx
@@ -0,0 +1,11 @@
+
+export const HeaderLogo = () => {
+    return (
+        <div className="self-center font-mono font-bold text-3xl text-white tracking-tighter">
+            <span>S</span>
+            <span className="hidden sm:inline-block">pace</span>
+            <span>F</span>
+            <span className="hidden sm:inline-block">light</span>
+        </div>
+    )
+}
