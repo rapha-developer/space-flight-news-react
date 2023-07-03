60938be3dcc9a63ddc23592cacd0954ce0836549
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-03 12:29:23 -0300

    [HomeArticle step:1] Add link to picture, title and summary. Each link redirect to full article. Specify image width and height. And change color of the article publishedAt

--- src/components/Home/HomeArticle.tsx
+++ src/components/Home/HomeArticle.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Article } from '../../types/article'
 
 type Props = {
@@ -7,14 +8,24 @@ type Props = {
 export const HomeArticle = ({ article }: Props) => {
     return (
         <div className="flex flex-col gap-4 py-4 border-b font-mono">
-            <img 
-                className="aspect-video object-cover"
-                src={article.imageUrl}
-                alt={article.title}
-            /> 
-            <h2 className="text-2xl">{article.title}</h2>
-            <p className="text-sm hidden lg:block">{article.summary}</p>
-            <p className="text-sm text-gray-400">
+            <Link 
+                to={`/article/${article.id}`}
+                preventScrollReset={true}
+            >
+                <picture>
+                <img 
+                    className="aspect-video object-cover"
+                    src={article.imageUrl}
+                    alt={article.title}
+                    width="1920"
+                    height="1080"
+                    loading='lazy'
+                /> 
+                </picture>
+                <h2 className="text-2xl">{article.title}</h2>
+                <p className="text-sm hidden lg:block">{article.summary}</p>
+            </Link>
+            <p className="text-sm text-gray-500">
                 {new Date(article.publishedAt).toLocaleString()}
             </p>
         </div>
