ebe6c4552e31b75ba99b74880ee863d68c1c6269
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-03 11:34:33 -0300

    [Header step:2] Hide HeaderSearchBar when actual location is not root

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { HeaderLogo } from './HeaderLogo'
 import { HeaderSearchBar } from './HeaderSearchBar'
 
 export const Header = () => {
+    const location = useLocation()
     return (
         <header className='bg-gray-800'>
             <div className="container flex gap-4 mx-auto px-4 py-2">
                 <Link className='self-center' to='/'>
                     <HeaderLogo />
                 </Link>
-                <HeaderSearchBar />
+                {location.pathname === '/' && <HeaderSearchBar />}
             </div>
         </header>
     )
