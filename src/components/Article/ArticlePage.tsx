7b70df8d5150b0ab702b5d34fca6a799af0ad5d1
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-03 12:21:04 -0300

    [ArticlePage step:1] create ArticlePage component. Responsible for show each article, useFetch function collect title, data, summary of the article from api

---
+++ src/components/Article/ArticlePage.tsx
@@ -0,0 +1,39 @@
+import { RocketLaunchIcon } from '@heroicons/react/24/solid'
+import { useParams } from 'react-router-dom'
+import { useMemo } from 'react'
+import type { Article } from '../../types/article'
+import { useFetch } from '../../hooks/useFetch'
+
+export const ArticlePage = () => {
+    const { id } = useParams<{id: string}>()
+
+    const fetchUrl = useMemo(() => {
+        return new URL(`https://api.spaceflightnewsapi.net/v3/articles/${id}`)
+    }, [id])
+    const { data: article, isLoading} = useFetch<Article>(fetchUrl)
+
+    return (
+        <div className="container mx-auto">
+            {isLoading || !article ? (
+                <RocketLaunchIcon className='mx-auto my-12 h-12 w-12 text-gray-400 animate-bounce'
+                />
+            ): (
+                <div className="px-4 lg:px-0">
+                    <img 
+                        className='mt-4 w-full aspect-video object-cover'
+                        src={article.imageUrl} 
+                        alt={article.title} 
+                        width="1920"
+                        height="1080"
+                        loading='lazy'
+                    />
+                    <p className="mt-2 text-sm text-gray-500">
+                        {new Date(article.publishedAt).toLocaleString()}
+                    </p>
+                    <h2 className="mt-4 text-4xl">{article.title}</h2>
+                    <p className="mt-4 text-base">{article.summary}</p>
+                </div>
+            )}
+        </div>
+    )
+}
