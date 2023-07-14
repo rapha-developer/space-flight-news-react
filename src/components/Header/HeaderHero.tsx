16ded9015c54010e4649f8e199c89a12b97adcc6
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-14 11:25:12 -0300

    [Header step:3] create HeaderHero component. Responsible for create hero section in HomePage. Hide when open another page

---
+++ src/components/Header/HeaderHero.tsx
@@ -0,0 +1,17 @@
+import { HeaderSearchBar } from "./HeaderSearchBar"
+
+type Props = {
+    title: string,
+    description: string
+}
+export const HeaderHero = ({title, description}: Props) => {
+    return (
+        <div className="container mt-20 flex flex-col gap-3 justify-center mx-auto">
+            <h1 className="font-mono font-semibold text-3xl text-white text-center">{title}</h1>
+            <p className='font-mono font-normal text-base text-white text-center'>
+                {description}
+            </p>
+            <HeaderSearchBar />
+        </div>
+    )
+}
