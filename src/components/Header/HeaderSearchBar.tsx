fdae7ac10382e53d27b93981f0ca8ac384a51b4e
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-14 11:31:42 -0300

    [Header step:4] Make SearchBar with better dark design.

--- src/components/Header/HeaderSearchBar.tsx
+++ src/components/Header/HeaderSearchBar.tsx
@@ -14,13 +14,13 @@ export const HeaderSearchBar = () => {
         []
     )
     return (
-        <div className="flex grow">
-            <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <MagnifyingGlassIcon className='h-4 w-4 text-gray-500' />
+        <div className="flex grow mt-8 sm:mt-24">
+            <div className="relative w-11/12 sm:w-full max-w-lg mx-auto">
+                <div className="absolute inset-y-0 right-0 flex items-center px-5 py-3 pointer-events-none  bg-cyan-500 rounded-md">
+                    <MagnifyingGlassIcon className='h-4 w-4 text-white font-bold' />
                 </div>
                 <input
-                    className='block p-2.5 pl-10 w-full z-20 text-lg text-gray-900 border-none focus-visible:outline-none'
+                    className='block p-2.5 pl-10 w-full z-20 text-lg text-white bg-dark-200 border-none rounded-lg focus:border-dark-300 focus:border-2 focus-visible:outline-none'
                     type="search" 
                     placeholder='Search'
                     onChange={onChangeHandler}
