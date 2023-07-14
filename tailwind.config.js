0d87540ee9a4229236ee3619662f239d35423989
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-14 11:11:10 -0300

    [colors step:2] configure tailwind theme with new three dark colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': {
+          '100': 'hsl(var(--color-dark-100))',
+          '200': 'hsl(var(--color-dark-200))',
+          '300': 'hsl(var(--color-dark-300))'
+        },
+      }
+    },
   },
   plugins: [],
 }
