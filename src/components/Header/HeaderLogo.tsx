1d60723827ccb8f2cdec40470fe95b098282e1f5
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-14 11:14:10 -0300

    [Header step:1] set explicit height and width to HeaderLogo component

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -4,6 +4,8 @@ export const HeaderLogo = () => {
         <img 
             src="/logo.svg" 
             alt="loremIpsum logo" 
+            height="40"
+            width="170"
             className='w-full'
         />
     )
