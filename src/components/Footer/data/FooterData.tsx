c977f79617da98f0e8e77b0559d3aa446c1247e2
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-19 11:34:39 -0300

    [Footer step:5] FooterData contains data used to fill various components within the footer

---
+++ src/components/Footer/data/FooterData.tsx
@@ -0,0 +1,25 @@
+
+export const footerAboutSection = {
+    about: "Space flight news React é uma aplicação web que procura e exibe as últimas noticias relacionadas ao espaço.",
+}
+export const footerFollowSection = {
+    title: "follow us"
+}
+export const footerInspiredSection = {
+    label: "Hoverex theme",
+    url: "https://preview.themeforest.net/item/hoverex-cryptocurrency-ico-wp-theme/full_screen_preview/21694781%20?utm_source=colorlib&irgwc=1&clickid=xorTX620DxyPTbWzq5VrjXetUkFzXcwgNXVXU00&iradid=275988&irpid=369282&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_369282&utm_medium=affiliate&utm_source=impact_radius&utm_medium=roundup&utm_campaign=futuristic"
+}
+export const footerInformationSection = {
+    job: {
+        title: "job",
+        description: "Frontend developer"
+    },
+    mail: {
+        title: "mail",
+        description: "contatosraphaelmoreira@gmail.com"
+    },
+    map: {
+        title: "find us",
+        description: "São paulo, brazil."
+    },
+}
