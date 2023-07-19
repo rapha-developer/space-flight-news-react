efdb50fcb3e89cea0f8ac30f4f745e96fb7bc6b0
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-19 11:58:47 -0300

    [App step:1] Import and use Footer component inside the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Header } from './components/Header'
 import { HomePage } from './components/Home/HomePage'
 import { ArticlePage } from './components/Article/ArticlePage'
+import { Footer } from './components/Footer/Footer'
 const App = () => {
   return (
-    <div className='bg-dark-100 min-h-screen'>
+    <div className='bg-dark-100 min-h-screen h-full'>
     <BrowserRouter>
       <Header />
       <Routes>
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/article/:id' element={<ArticlePage />} />
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
     </div>
   )
