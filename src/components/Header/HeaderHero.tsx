ad97bdcd47bd4811c6662859e7b89ed7429f7490
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-07-20 16:45:29 -0300

    [HeaderHero step:1] Change font-family property from mono to inter

--- src/components/Header/HeaderHero.tsx
+++ src/components/Header/HeaderHero.tsx
@@ -8,7 +8,7 @@ export const HeaderHero = ({title, description}: Props) => {
     return (
         <div className="container mt-20 flex flex-col gap-3 justify-center mx-auto">
             <h1 className="font-mono font-semibold text-3xl text-white text-center">{title}</h1>
-            <p className='font-mono font-normal text-base text-white text-center'>
+            <p className='font-inter font-normal text-base text-white text-center'>
                 {description}
             </p>
             <HeaderSearchBar />
